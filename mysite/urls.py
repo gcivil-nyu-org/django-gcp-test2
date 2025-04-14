fbfbc0cc513ab4f841823274f00898db041cb953
shreyas2499 <shreyas2499@gmail.com>
2025-04-13 20:05:12 -0400

    Added dummy route to test deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1> Hello from Travis Deployed App on GCP!</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # override default page
 ]
